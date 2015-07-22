96d28c627dcb4f24668d53d818918dc7c39600ca
arampur <arampur@csu.fullerton.edu>
2015-07-21 18:29:21 -0700

    latest added

--- Visual Studio 2/Project1-string/stringclass.cpp
+++ Visual Studio 2/Project1-string/stringclass.cpp
@@ -2,37 +2,70 @@
 #include<string>
 #include<algorithm>
 using namespace std;
+//program to understand all operations in string...
 int main(){
-	cout << "Enter the sample string: " << endl;
+	cout << "Enter your string: " << endl;
 	string mystring;
 	getline(cin, mystring);
-	//searching for particular keyword in string..
-	string findstring;
-	cout << "Enter the string you want to find: " << endl;
-	cin >> findstring;
-	size_t substring = mystring.find(findstring, 0);
-	if (substring != string::npos)
-		cout << "substring was found at the position : " << substring << endl;
-	else
-		cout << "substring was not found.." << endl;
-	cout << "\n";
-	//finding all occurence of given string
-	size_t substringpos = mystring.find(findstring, 0);
-	cout << "finding all occurences of " << findstring << " in the sample string.." << endl;
-	while (substringpos != string::npos){
-		cout << "substring was found at position : " << substringpos << endl;
-
-		size_t searchposition = substringpos + 1;
-		substringpos = mystring.find(findstring, searchposition);
-	}
-
-	cout << "reversing contents of the string: ";
-	reverse(mystring.begin(), mystring.end());
-	cout << "\n";
-	cout << "After reversing the string : " << endl;
-	cout << "\n";
-	transform(mystring.begin(), mystring.end(), mystring.begin(),toupper);
-	cout << "After changing the case of the string : " << endl;
+	cout << "You entered: ";
 	cout << mystring << endl;
+	cout << "\n";
+	
+	//copying one string to another
+	string string2(mystring);
+	cout << "Second string after copying from the sample string is now : " << endl;
+	cout << string2 << endl;
+	cout << "\n";
+	
+	//finding substring in sample string
+	cout << "Finding occurence of day in the sample string.." << endl;
+	size_t substring = mystring.find("day", 0);
+	if (substring != string::npos)
+		cout << "substring '\day\'" << " was found in the sample string" << endl;
+	else
+		cout << "string you are looking for not in sample string.." << endl;
+	cout << "\n";
+	
+	//finding all occurences of day in sample string
+	string findstr;
+	cout << "enter the string you want to find.." << endl;
+	getline(cin, findstr);
+	cout << "finding all occurences of  "<<findstr<<" in sample string.." << endl;
+	size_t findall = mystring.find(findstr, 0);
+	while (findall != string::npos){
+		cout << "string " << findstr << " was found in the sample string" << endl;
+		size_t stringposition = findall + 1;
+		findall = mystring.find(findall, stringposition);
+	}
+	cout << "\n";
+
+	string newstring;
+	cout << "concatenating sample string to new string.." << endl;
+	newstring.append(mystring);
+	cout << newstring << endl;
+	cout << "\n";
+	
+	//reversing the contents of the string
+	cout << "reversing the contents of the new string.." << endl;
+	reverse(newstring.begin(), newstring.end());
+	cout << "After reversing contents: " << newstring << endl;
+	cout << "\n";
+
+	//comparing two string
+	if (mystring == string2)
+		cout << "strings " << mystring <<" and "<< string2<<" are equal" << endl;
+	else
+		cout << "strings are not equal" << endl;
+	cout << "\n";
+
+	//transforming to upper case
+	transform(newstring.begin(), newstring.end(), newstring.begin(), toupper);
+	cout << "After conversion to uppercase, the new string is now : " << newstring << endl;
+	cout << "\n";
+
+	//erasing string contents
+	string2.erase();
+	if (string2.empty())
+		cout << "second string is empty..." << endl;
 	return 0;
 }
